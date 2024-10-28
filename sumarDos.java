d77227b3f993a642776cb9bb356faf3d925072a9
David Duirs <61534026+dduirs@users.noreply.github.com>
2024-10-28 20:49:10 +0100

    Create sumarDos.java

---
+++ sumarDos.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().sumarDos(1,465));
+    }
+    public int sumarDos(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+}
