1a617a10b045b9d219f77016e49749d5df20179a
David Duirs <61534026+dduirs@users.noreply.github.com>
2024-10-28 20:33:42 +0100

    Create mostrarPi.java

---
+++ mostrarPi.java
@@ -0,0 +1,9 @@
+public class Main {
+    double pi = 3.1416;
+    public static void main(String[] args) {
+        new Main().mostrarPI();
+    }
+    public void mostrarPI(){
+        System.out.print("pi = "+pi);
+    }
+}
